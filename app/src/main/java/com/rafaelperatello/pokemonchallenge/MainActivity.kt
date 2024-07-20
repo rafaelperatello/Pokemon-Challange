e3d4db95b8504422f3d4ae021eb1b4d245df18f9
Rafael Peratello <rafaelperatello@gmail.com>
2024-07-20 15:49:45 -0300

    Type safe navigation

--- app/src/main/java/com/rafaelperatello/pokemonchallenge/MainActivity.kt
+++ app/src/main/java/com/rafaelperatello/pokemonchallenge/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.Keep
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,16 +20,22 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.rounded.Favorite
-import androidx.compose.material.icons.rounded.Home
-import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material.icons.outlined.Apps
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -46,6 +55,7 @@ import com.rafaelperatello.pokemonchallenge.ui.theme.PokemonChallengeTheme
 import com.rafaelperatello.pokemonchallenge.ui.widget.PokemonAppBar
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
 
@@ -75,6 +85,20 @@ class MainActivity : ComponentActivity() {
 fun PokemonScaffold() {
     val navController = rememberNavController()
 
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute by remember {
+        derivedStateOf {
+            currentBackStackEntry?.destination?.route?.let { route ->
+                when (route) {
+                    MainRoutes.Home::class.qualifiedName -> MainRoutes.Home
+                    MainRoutes.Favorites::class.qualifiedName -> MainRoutes.Favorites
+                    MainRoutes.Settings::class.qualifiedName -> MainRoutes.Settings
+                    else -> null
+                }
+            }
+        }
+    }
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -100,20 +124,32 @@ fun PokemonScaffold() {
 @Composable
 fun MainNavHost(
     navController: NavHostController,
-    startDestination: String
+    startDestination: MainRoutes
 ) {
     NavHost(
         navController = navController,
         startDestination = startDestination
     ) {
-        composable(route = BottomBarScreen.Home.route) {
+        composable<MainRoutes.Home>() {
             HomeScreen()
         }
-        composable(route = BottomBarScreen.Favorites.route) {
-            Text(text = "Favorites")
+        composable<MainRoutes.Favorites>() {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Favorites")
+            }
         }
-        composable(route = BottomBarScreen.Settings.route) {
-            Text(text = "Settings")
+        composable<MainRoutes.Settings>() {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Settings")
+            }
         }
     }
 }
@@ -124,36 +160,35 @@ private fun HomeBottomBar(
 ) {
     val currentDestination = navController.currentBackStackEntryAsState().value?.destination
 
-    val screens = listOf(
-        BottomBarScreen.Home,
-        BottomBarScreen.Favorites,
-        BottomBarScreen.Settings
-    )
-
     NavigationBar(
         containerColor = MaterialTheme.colorScheme.primaryContainer,
         contentColor = MaterialTheme.colorScheme.primary,
-        modifier = Modifier
-            .windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal))
+        modifier = Modifier.windowInsetsPadding(
+            WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal)
+        )
     ) {
-        screens.forEach { destination ->
+        mainScreens.forEach { destination ->
             val selected =
-                currentDestination?.hierarchy?.any { it.route == destination.route } == true
+                currentDestination?.hierarchy?.any {
+                    it.route == destination.route::class.qualifiedName.orEmpty()
+                } == true
 
             NavigationBarItem(
                 selected = selected,
                 onClick = {
                     navController.navigate(destination.route) {
-                        popUpTo(navController.graph.findStartDestination().id)
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
                         launchSingleTop = true
+                        restoreState = true
                     }
                 },
                 icon = {
-                    val icon = if (selected) {
-                        destination.selectedIcon
-                    } else {
-                        destination.unselectedIcon
-                    }
+                    val icon =
+                        if (selected) destination.selectedIcon
+                        else destination.unselectedIcon
+
                     Icon(
                         imageVector = icon,
                         modifier = Modifier.size(16.dp),
@@ -171,30 +206,51 @@ private fun HomeBottomBar(
     }
 }
 
-sealed class BottomBarScreen(
-    val route: String,
+private val mainScreens = listOf(
+    BottomBarItems.Home,
+    BottomBarItems.Favorites,
+    BottomBarItems.Settings
+)
+
+@Keep
+@Serializable
+sealed class MainRoutes {
+
+    @Serializable
+    data object Home : MainRoutes()
+
+    @Serializable
+    data object Favorites : MainRoutes()
+
+    @Serializable
+    data object Settings : MainRoutes()
+}
+
+sealed class BottomBarItems(
+    val route: MainRoutes,
     val labelRes: Int,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector
 ) {
-    data object Home : BottomBarScreen(
-        route = "home",
+
+    data object Home : BottomBarItems(
+        route = MainRoutes.Home,
         labelRes = R.string.home,
-        selectedIcon = Icons.Rounded.Home,
-        unselectedIcon = Icons.Filled.Home
+        selectedIcon = Icons.Filled.Home,
+        unselectedIcon = Icons.Outlined.Home
     )
 
-    data object Favorites : BottomBarScreen(
-        route = "favorites",
+    data object Favorites : BottomBarItems(
+        route = MainRoutes.Favorites,
         labelRes = R.string.favorite,
-        selectedIcon = Icons.Rounded.Favorite,
-        unselectedIcon = Icons.Filled.Favorite
+        selectedIcon = Icons.Filled.Favorite,
+        unselectedIcon = Icons.Outlined.FavoriteBorder
     )
 
-    data object Settings : BottomBarScreen(
-        route = "settings",
+    data object Settings : BottomBarItems(
+        route = MainRoutes.Settings,
         labelRes = R.string.settings,
-        selectedIcon = Icons.Rounded.Settings,
-        unselectedIcon = Icons.Filled.Settings
+        selectedIcon = Icons.Filled.Settings,
+        unselectedIcon = Icons.Outlined.Settings
     )
 }
