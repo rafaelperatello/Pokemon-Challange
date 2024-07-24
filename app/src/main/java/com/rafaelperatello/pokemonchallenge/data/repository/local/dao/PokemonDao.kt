f281164c3eb95e7a998bcb18b5420a3c9949e68d
Rafael Peratello <rafaelperatello@gmail.com>
2024-07-24 13:36:05 -0300

    Update Dao

--- app/src/main/java/com/rafaelperatello/pokemonchallenge/data/repository/local/dao/PokemonDao.kt
+++ app/src/main/java/com/rafaelperatello/pokemonchallenge/data/repository/local/dao/PokemonDao.kt
@@ -6,15 +6,22 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Update
 import com.rafaelperatello.pokemonchallenge.data.repository.local.entity.PokemonEntity
 import com.rafaelperatello.pokemonchallenge.data.repository.local.pojo.ShallowPokemonPojo
 
 @Dao
 internal interface PokemonDao {
-    // Todo check how to keep isFavorite when updating
+
+    @Transaction
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertAllIgnoring(pokemons: List<PokemonEntity>)
+    suspend fun insertAllIgnoring(pokemons: List<PokemonEntity>) : List<Long>
+
+    // Todo check how to keep isFavorite when updating
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllReplacing(pokemons: List<PokemonEntity>)
 
     // Todo check how to keep isFavorite when updating
     @Update(onConflict = OnConflictStrategy.REPLACE)
@@ -26,14 +33,14 @@ internal interface PokemonDao {
     @Query("DELETE FROM pokemon")
     suspend fun deleteAll()
 
-    @Query("SELECT * FROM pokemon")
+    @Query("SELECT * FROM pokemon ORDER BY id ASC")
     suspend fun getAll(): List<PokemonEntity>
 
     @Query(
         """
         SELECT id, pokemon_id, name, number, image_small, image_large 
         FROM pokemon 
-        ORDER BY id DESC
+        ORDER BY id ASC
         LIMIT :limit 
         OFFSET :offset
         """,
